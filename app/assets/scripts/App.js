96249fcbdd156642bdedf875876e3fb5f6920f99
VaX <netdumpster14@gmail.com>
2017-10-05 22:47:59 -0400

    Completed Sticky Header.

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,7 +1,9 @@
 import $ from 'jquery';
 import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
+import StickyHeader from './modules/StickyHeader';
 
 let mobileMenu = new MobileMenu(); // new object instance of class
 new RevealOnScroll($(".feature-item"), "85%");
 new RevealOnScroll($(".testimonial"), "60%");
+let stickyHeader = new StickyHeader(); // new object instance of class
